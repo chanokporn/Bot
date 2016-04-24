d898eee9a80eb8c507a0d56f9ceae36053e0125c
chanokporn <porpai.chanok2537@gmail.com>
2016-04-24 14:24:03 +0700

    webhook

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ var app = express()
 app.get('/', function (req, res) {
   res.send('hello word')
 })
+app.get('/webhook/', function (req, res) {
+  if (req.query['hub.verify_token'] === 'pai') {
+    res.send(req.query['hub.challenge'])
+  }
+  res.send('Error, wrong validation token')
+})
 
 app.set('port', (process.env.PORT || 3000))
 
