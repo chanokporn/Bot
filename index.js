363ec74fe0a5404e4305fd92078d6e6390196601
chanokporn <porpai.chanok2537@gmail.com>
2016-04-24 14:09:40 +0700

    index

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ app.get('/', function (req, res) {
   res.send('hello word')
 })
 
-app.set('port', (process.env.port || 3000))
+app.set('port', (process.env.PORT || 3000))
 
 app.listen(app.get('port'), function () {
   console.log('Server Start at port', app.get('port'))
